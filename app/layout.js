36c7c80ab97770c57796ede74dfadc0abe71f6d1
Vaibhav <vaibhavjam86@gmail.com>
2025-02-05 10:16:25 +0530

    added circular progressbar

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,19 @@
 import "./globals.css";
 import CustomCursor from "@/components/CustomCursor";
+import CircularProgress from "@/components/CircularProgress";
 
 export const metadata = {
-  title: "Your Project Title",
-  description: "Your project description",
+  title: "Vaibhav Jamdhade Portfolio",
+  description: "Frontend Developer",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="text-white bg-[#1d1d1d] text-sm  overflow-x-hidden relative ">
+      <body className="text-white bg-[#1d1d1d] text-sm overflow-x-hidden relative">
         <CustomCursor />
         {children}
+        <CircularProgress />
       </body>
     </html>
   );
