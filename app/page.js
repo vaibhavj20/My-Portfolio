3f795c6d31045320b2a8c5b075f2b82382072385
Vaibhav <vaibhavjam86@gmail.com>
2025-02-05 10:16:45 +0530

    added next sections

--- app/page.js
+++ app/page.js
@@ -1,15 +1,31 @@
+"use client";
+import { useState, useEffect } from "react";
 import Background from "../components/Backgorund";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import Main from "@/components/Main";
+import Loader from "@/components/Loader";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
-    <main className="">
-      <Background />
-      <div className=" px-3 md:px-20 lg:px-52">
-        <Navbar />
-        <Hero />
-      </div>
-    </main>
+    <>
+      {isLoading && <Loader setIsLoading={setIsLoading} />}
+      <main
+        className={`relative min-h-screen w-full transition-transform duration-1000 ease-in-out ${
+          isLoading ? "translate-y-[100vh]" : "translate-y-0"
+        }`}
+      >
+        <Background />
+        <div className="relative md:px-20 lg:px-52">
+          <Navbar />
+          <Hero />
+          <div className="">
+            <Main />
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
